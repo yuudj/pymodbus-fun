1593603e7dbaddc1b284255cbe9a11397f23e286
yuudj <yuudj@hotmail.com>
2023-12-17 22:41:08 -0300

    modbus client code

---
+++ client.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+"""Pymodbus synchronous client example.
+
+An example of a single threaded synchronous client.
+
+usage: simple_client_async.py
+
+All options must be adapted in the code
+The corresponding server must be started before e.g. as:
+    python3 server_sync.py
+"""
+
+# --------------------------------------------------------------------------- #
+# import the various client implementations
+# --------------------------------------------------------------------------- #
+import pymodbus.client as ModbusClient
+from pymodbus import (
+    ExceptionResponse,
+    Framer,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
+
+
+def run_sync_simple_client(comm, host, port, framer=Framer.SOCKET):
+    """Run sync client."""
+    # activate debugging
+    pymodbus_apply_logging_config("DEBUG")
+
+    print("get client")
+    if comm == "tcp":
+        client = ModbusClient.ModbusTcpClient(
+            host,
+            port=port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,y
+            # close_comm_on_error=False,
+            # strict=True,
+            # source_address=("localhost", 0),
+        )
+    elif comm == "udp":
+        client = ModbusClient.ModbusUdpClient(
+            host,
+            port=port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # close_comm_on_error=False,
+            # strict=True,
+            # source_address=None,
+        )
+    elif comm == "serial":
+        client = ModbusClient.ModbusSerialClient(
+            port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # close_comm_on_error=False,.
+            # strict=True,
+            baudrate=9600,
+            bytesize=8,
+            parity="N",
+            stopbits=1,
+            # handle_local_echo=False,
+        )
+    elif comm == "tls":
+        client = ModbusClient.ModbusTlsClient(
+            host,
+            port=port,
+            framer=Framer.TLS,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # close_comm_on_error=False,
+            # strict=True,
+            # sslctx=None,
+            certfile="../examples/certificates/pymodbus.crt",
+            keyfile="../examples/certificates/pymodbus.key",
+            # password=None,
+            server_hostname="localhost",
+        )
+    else:  # pragma no cover
+        print(f"Unknown client {comm} selected")
+        return
+
+    print("connect to server")
+    client.connect()
+
+    print("get and verify data")
+    try:
+        #rr = client.read_coils(3, 1, slave=1)
+        rr = client.read_holding_registers(3)
+    except ModbusException as exc:
+        print(f"Received ModbusException({exc}) from library")
+        client.close()
+        return
+    if rr.isError():  # pragma no cover
+        print(f"Received Modbus library error({rr})")
+        client.close()
+        return
+    if isinstance(rr, ExceptionResponse):  # pragma no cover
+        print(f"Received Modbus library exception ({rr})")
+        # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
+        client.close()
+    print(rr.registers)
+    print("close connection")  # pragma no cover
+    client.close()  # pragma no cover
+
+
+if __name__ == "__main__":
+    run_sync_simple_client("serial", "" , "/dev/ttyUSB0",Framer.RTU)  # pragma: no cover
